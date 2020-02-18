603743dc05a84c4d9d1bd3bcf33d4a3d45b46ba5
Steven Linn <smlucf@gmail.com>
2020-02-17 19:03:11 -0500

    Move node package management responsibility to Docker and support SCSS files in React

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@
  */
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 import CommitList from "./components/CommitList";
 
